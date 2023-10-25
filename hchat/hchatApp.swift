83d72ad3d9335bdec09178d6555d5365803e0419
Michael Sheslow <msheslow@gmail.com>
2023-10-25 04:49:36 -0600

    Initial Commit

---
+++ hchat/hchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  hchatApp.swift
+//  hchat
+//
+//  Created by msheslow on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct hchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
